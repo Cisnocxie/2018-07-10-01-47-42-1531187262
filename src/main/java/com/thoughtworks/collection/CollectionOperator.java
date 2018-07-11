ca1ba8b4097d9a7ac713f3f198991cfeb37365e0
Cisnoc Xie <502021156@qq.com>
2018-07-12 01:06:48 +0800

    pass getListByInterval(int left, int right)

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,11 +2,15 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        return left < right ? IntStream.rangeClosed(left, right).mapToObj(i -> new Integer(i)).collect(Collectors.toList()) : IntStream.rangeClosed(right, left).mapToObj(i -> new Integer(i)).sorted(Collections.reverseOrder()).collect(Collectors.toList());
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
