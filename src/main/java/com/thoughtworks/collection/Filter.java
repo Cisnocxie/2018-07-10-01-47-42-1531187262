9f4b5bb96fee7ffcdf0c83d0c737833f18b6ebf7
Cisnoc Xie <502021156@qq.com>
2018-07-15 19:02:28 +0800

    refactory Filter.getCommonElements

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -23,14 +23,9 @@ public class Filter {
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        return firstList.stream().filter(i -> {
-            for (Integer j : secondList) {
-                if (i == j) {
-                    return true;
-                }
-            }
-            return false;
-        }).collect(Collectors.toList());
+        return firstList.stream().filter(i ->
+                secondList.stream().anyMatch(i::equals))
+                .collect(Collectors.toList());
     }
 
     public List<Integer> getDifferentElements() {
